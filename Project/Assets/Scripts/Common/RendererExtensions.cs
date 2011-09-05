835adb2927ea7e322cc4acaf83158537b9cca7d6
Tomas Rimkus <sandshrew@gmail.com>
2011-09-05 20:16:19 +0200

    RendererExtension class has a method for checking if some object is visible by particular camera.

---
+++ Project/Assets/Scripts/Common/RendererExtensions.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public static class RendererExtensions
+{
+    public static bool IsVisibleFrom(this Renderer renderer, Camera camera)
+    {
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(camera);
+        return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
+    }
+}
