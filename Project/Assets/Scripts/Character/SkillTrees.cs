84b7be6bf19e2aa7113c0eab6db48081b65daf57
Tomas Rimkus <sandshrew@gmail.com>
2012-02-09 20:10:02 +0100

    Stormer skill tree is now static.

--- Project/Assets/Scripts/Character/SkillTrees.cs
+++ Project/Assets/Scripts/Character/SkillTrees.cs
@@ -11,10 +11,15 @@ public static class SkillTrees
     public const int btnWidth = 80; //120
     public const int btnHeight = 50; //60
 
+    private static SkillTree stormerSkills = null;
+
     public static SkillTree StormerSkills
     {
         get
         {
+            if (stormerSkills != null)
+                return stormerSkills;
+
             Dictionary<string, Skill> skills;
             Dictionary<Skill, Vector2> skillPos;
             skills = new Dictionary<string,Skill>();
@@ -48,12 +53,12 @@ public static class SkillTrees
 
             foreach(Skill skill in skillPos.Keys)
                 skills.Add(skill.Name, skill);
+                
 
-            SkillTree skillTree;
-            skillTree = new SkillTree(Helper.getImage("StormerSkills"), skills,
-                skillPos);
+            stormerSkills = new SkillTree(Helper.getImage("StormerSkills"), 
+                skills, skillPos);
 
-            return skillTree;
+            return stormerSkills;
         }
     }
 }
