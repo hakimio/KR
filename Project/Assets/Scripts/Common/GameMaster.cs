41df0614e56b6eb68369471b09f027cf41861f83
Tomas Rimkus <sandshrew@gmail.com>
2012-03-21 16:02:56 +0100

    GameMaster: majore refactoring, moved stuff that shouldn't be here to other scripts, also it is from now on abstract and each level has its own game master implementation.

--- Project/Assets/Scripts/Common/GameMaster.cs
+++ Project/Assets/Scripts/Common/GameMaster.cs
@@ -1,128 +1,44 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using System;
 
-public class GameMaster : MonoBehaviour
+public abstract class GameMaster : MonoBehaviour
 {
-	public GameObject playerChar;
-	public GameObject gameSettings;
-    public List<Conversation> dialogs;
-    public List<Message> messages;
-    public static GameMaster instance;
+    public bool inCombat = false;
+    public static GameMaster instance = null;
     public List<BaseChar> characters;
     public BaseChar selectedChar;
-
-	private GameObject pc;
-    bool loaded = false;
 	
 	void Awake()
 	{
+        if (instance != null)
+        {
+            characters = instance.characters;
+            selectedChar = instance.selectedChar;
+            foreach (BaseChar character in characters)
+                character.LostHP = 0;
+            instantiateCharacters();
+            Destroy(gameObject);
+            return;
+        }
+
         instance = this;
-        loadDialogs();
-        loadMessages();
-		MyCamera.acquireCamera();
-        MyCamera.instance.MouseYRot = 30;
-        instantiatePC();
+        DontDestroyOnLoad(this);
+        
         characters = new List<BaseChar>();
+        characters.Add(Characters.TheRobot);
         characters.Add(Characters.EricFrost);
-        characters.Add(Characters.FrostEric);
+        characters.Add(Characters.Lerpz);
         selectedChar = characters[0];
-        Messenger.AddListener("MessageBox Ready", showIntroMessage);
+
+        instantiateCharacters();
 	}
 
-    void showIntroMessage()
+    void Update()
     {
-        foreach (Message message in messages)
-            if (message.Name.Equals("intro"))
-            {
-                MessageBox.instance.showMessage(message.Content);
-                break;
-            }
-    }
-
-    private void loadDialogs()
-    {
-        dialogs = new List<Conversation>();
-        Uri uri = new Uri(Directory.GetCurrentDirectory() +
-            "/Assets/Dialogs");
-        foreach (String file in Directory.GetFiles(uri.LocalPath))
-        {
-            if (System.IO.Path.GetExtension(file).Equals(".meta"))
-                continue;
-            Conversation conv = XmlReader.read(file);
-            dialogs.Add(conv);
-        }
-    }
-
-    private void loadMessages()
-    {
-        messages = new List<Message>();
-        Uri uri = new Uri(Directory.GetCurrentDirectory() +
-            "/Assets/Messages");
-        foreach (String file in Directory.GetFiles(uri.LocalPath))
-        {
-            if (System.IO.Path.GetExtension(file).Equals(".meta"))
-                continue;
-            string name = System.IO.Path.GetFileNameWithoutExtension(file);
-            string content = TxtReader.read(file);
-            messages.Add(new Message(name, content));
-        }
-    }
-
-    private void instantiatePC()
-    {
-        GameObject spawnPoint = GameObject.Find("Player Spawn Point");
-
-        if (spawnPoint == null)
-        {
-            spawnPoint = new GameObject("Player Spawn Point");
-            spawnPoint.transform.position = new Vector3(230, 0, 210);
-        }
-
-        pc = GameObject.Find("Player Character");
-
-        if (pc == null)
-        {
-            pc = (GameObject)Instantiate(playerChar,
-                spawnPoint.transform.position, Quaternion.identity);
-            pc.name = "Player Character";
-            pc.transform.localScale = new Vector3(3f, 3f, 3f);
-        }
-        MyCamera.instance.TargetLookAt = pc.transform;
-        GameObject.Destroy(spawnPoint);
-    }
-
-	void Start ()
-	{
-        Messenger<ItemSlots>.Broadcast("ItemSlotChanged", ItemSlots.Slot1);
-		//loadChar();
-	}
-	
-	private void loadChar ()
-	{
-		GameObject gs = GameObject.Find("Game Settings");
-		if (gs == null)
-		{
-			gs = (GameObject)Instantiate(gameSettings, Vector3.zero, 
-                Quaternion.identity);
-			gs.name = "Game Settings";
-		}
-
-		GameSettings gsClass = gs.GetComponent<GameSettings>();
-		gsClass.loadChar();
-	}
-	
-	void Update()
-	{
         if (Input.GetKeyUp(KeyCode.Escape))
-            InGameMenu.instance.toggleVisibility();
-        if (!loaded && Application.GetStreamProgressForLevel("Level1") == 1)
-        {
-            HUD.instance.addMessage("Game loaded " + 
-                DateTime.Now.ToString("HH:mm"));
-            loaded = true;
-        }
-	}
+            InGameMenu.instance.enabled = !InGameMenu.instance.enabled;
+    }
+
+    protected abstract void instantiateCharacters();
 }
