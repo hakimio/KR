e94e199ec04b9703a75d30921263eb2d14961c22
Tomas Rimkus <sandshrew@gmail.com>
2012-03-21 15:20:52 +0100

    TileBehavior: disallow character movement in some cases (GUI windows, enemy turn and so on); show selected char and enemy walking range using different color tiles; check if player does not try to use more movement ponts than there are left; plus some new helper methods.

--- Project/Assets/Scripts/Arena/TileBehaviour.cs
+++ Project/Assets/Scripts/Arena/TileBehaviour.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TileBehaviour: MonoBehaviour
 {
@@ -7,6 +8,39 @@ public class TileBehaviour: MonoBehaviour
     public Material OpaqueMaterial;
     public Material defaultMaterial;
     Color orange = new Color(255f / 255f, 127f / 255f, 0, 127f/255f);
+    bool movementEnabled = true;
+
+    void Awake()
+    {
+        Messenger.AddListener("walkableTilesChanged", walkableTilesChanged);
+        Messenger.AddListener("monsterTilesChanged", walkableTilesChanged);
+        Messenger<bool>.AddListener("enable movement", enableMovement);
+    }
+
+    void walkableTilesChanged()
+    {
+        if (!tile.Passable || this == GridManager.instance.originTileTB)
+            return;
+
+        List<Tile> charTiles = CombatManager.instance.walkableTiles;
+        List<Tile> monsterTiles = AI.instance.monsterTiles;
+        if (monsterTiles.Contains(tile) && charTiles.Contains(tile))
+            changeColor(new Color(71f / 255f, 60f / 255f, 139f / 255f));
+        else if (monsterTiles.Contains(tile))
+            changeColor(new Color(0, 56f / 255f, 168f / 255f));
+        else if (charTiles.Contains(tile))
+            changeColor(new Color(0, 100f / 255f, 0, 120f / 255f));
+        else
+        {
+            renderer.material = defaultMaterial;
+            renderer.material.color = Color.white;
+        }
+    }
+
+    void enableMovement(bool enable)
+    {
+        movementEnabled = enable;
+    }
 
     void OnMouseEnter()
     {
@@ -20,7 +54,12 @@ public class TileBehaviour: MonoBehaviour
 
     void OnMouseOver()
     {
-        if (CharacterMovement.instance.IsMoving)
+        if (!movementEnabled)
+            return;
+        BaseChar selectedChar = GameMaster.instance.selectedChar;
+        CharacterMovement cm = selectedChar.gameObject.
+            GetComponent<CharacterMovement>();
+        if (cm.IsMoving)
             return;
 
         if (Input.GetMouseButtonUp(1))
@@ -38,7 +77,17 @@ public class TileBehaviour: MonoBehaviour
         }
         if (Input.GetMouseButtonUp(0))
         {
-            tile.Passable = true;
+            if (!tile.Passable)
+                return;
+            CombatManager CM = CombatManager.instance;
+            if (!CM.playersTurn)
+                return;
+
+            if (!CM.walkableTiles.Contains(tile))
+            {
+                HUD.instance.addMessage("Not enough steps left");
+                return;
+            }
 
             TileBehaviour originTileTB = GridManager.instance.originTileTB;
             if (this == originTileTB || originTileTB == null)
@@ -90,10 +139,17 @@ public class TileBehaviour: MonoBehaviour
     {
         GridManager.instance.selectedTile = null;
         if (tile.Passable && this != GridManager.instance.destTileTB
-            && this != GridManager.instance.originTileTB)
+            && this != GridManager.instance.originTileTB
+            && !CombatManager.instance.walkableTiles.Contains(tile))
         {
             this.renderer.material = defaultMaterial;
             this.renderer.material.color = Color.white;
+        }
+        else if (CombatManager.instance.walkableTiles.Contains(tile)
+            && tile.Passable && this != GridManager.instance.destTileTB
+            && this != GridManager.instance.originTileTB)
+        {
+            changeColor(new Color(0, 100f / 255f, 0, 120f / 255f));
         }
     }
 
@@ -104,5 +160,26 @@ public class TileBehaviour: MonoBehaviour
             tile.Passable = false;
             changeColor(Color.gray);
         }
+    }
+
+    public void setAsOrigin()
+    {
+        Color blue = Color.blue;
+        blue.a = 158f / 255f;
+        changeColor(blue);
+        GridManager.instance.originTileTB = this;
+    }
+
+    public void setAsNormal()
+    {
+        renderer.material = defaultMaterial;
+        renderer.material.color = Color.white;
+        tile.Passable = true;
+    }
+
+    public void setAsImpassable()
+    {
+        changeColor(Color.gray);
+        tile.Passable = false;
     }
 }
