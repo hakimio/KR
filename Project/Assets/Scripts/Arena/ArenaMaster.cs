49e776a6eabc858d5d20fa5d2721ed1eb0ebf055
Tomas Rimkus <sandshrew@gmail.com>
2012-03-21 16:49:17 +0100

    ArenaMaster: implements GameMaster and is used to position playable characters.

---
+++ Project/Assets/Scripts/Arena/ArenaMaster.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ArenaMaster: GameMaster
+{
+    public List<GameObject> characterGOs;
+
+    protected override void instantiateCharacters()
+    {
+        instance.inCombat = true;
+        GameObject spawnPoint;
+        for (int i = 1; i <= characters.Count; i++)
+        {
+            spawnPoint = GameObject.Find("Spawn point " + i);
+            Vector3 pos = spawnPoint.transform.position;
+            GameObject go = (GameObject)Instantiate(characterGOs[i - 1]);
+            pos.y = go.transform.position.y;
+            go.transform.position = pos;
+            characters[i - 1].gameObject = go;
+            GameObject.Destroy(spawnPoint);
+        }
+    }
+}
