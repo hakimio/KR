820a21960029844b9367219a655b098347cb7c1a
Tomas Rimkus <sandshrew@gmail.com>
2012-04-07 22:12:34 +0200

    Level1Master: script can now be used to instantiate more than one player character in the scene.

--- Project/Assets/Scripts/Common/Level1Master.cs
+++ Project/Assets/Scripts/Common/Level1Master.cs
@@ -1,19 +1,27 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Level1Master: GameMaster
 {
-    public GameObject characterGO;
+    public List<GameObject> characterGOs;
 
     protected override void instantiateCharacters()
     {
-        GameObject spawnPoint = GameObject.Find("Spawn Point");
-        GameObject pc = (GameObject)Instantiate(characterGO,
-                spawnPoint.transform.position, Quaternion.identity);
-        pc.name = "Player Character";
-        pc.transform.localScale = new Vector3(3f, 3f, 3f);
-        MyCamera.instance.Target = pc.transform.Find("CameraTarget");
-        characters[1].gameObject = pc;
-        GameObject.Destroy(spawnPoint);
+        GameObject parent = new GameObject("Characters");
+        GameObject spawnPoint;
+        for (int i = 1; i <= characters.Count; i++)
+        {
+            spawnPoint = GameObject.Find("Spawn point " + i);
+            Vector3 pos = spawnPoint.transform.position;
+            GameObject go = (GameObject)Instantiate(characterGOs[i - 1]);
+            go.name = characters[i - 1].charName;
+            go.transform.position = pos;
+            characters[i - 1].gameObject = go;
+            go.transform.parent = parent.transform;
+            if (i == 1)
+                MyCamera.instance.setTarget(go.transform.Find("CameraTarget"));
+            GameObject.Destroy(spawnPoint);
+        }
     }
 }
