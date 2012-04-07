d0d327c26826b85273742f9b93f47e6843f93f58
Tomas Rimkus <sandshrew@gmail.com>
2012-04-07 22:20:49 +0200

    AIAnimation: character animation now depends on movement speed.

--- Project/Assets/Scripts/A star pathfinding/Controlers/AIAnimation.cs
+++ Project/Assets/Scripts/A star pathfinding/Controlers/AIAnimation.cs
@@ -1,29 +1,32 @@
 using UnityEngine;
 using System.Collections;
 
-public class AIAnimation : MonoBehaviour {
-	public float minimumRunSpeed = 1.0F;
-	
-	public void Start () {
-		// Set all animations to loop
-		animation.wrapMode = WrapMode.Loop;
-	
-		// Except our action animations, Dont loop those
-		animation["jump_pose"].wrapMode = WrapMode.Once;
-		
-		// Put idle and run in a lower layer. They will only animate if our action animations are not playing
-		animation["idle"].layer = -1;
-		animation["walk"].layer = -1;
-		animation["run"].layer = -1;
-		
-		animation.Stop();
-	}
-	
-	public void SetSpeed (float speed) {
-		if (speed > minimumRunSpeed) {
-			animation.CrossFade("run");
-		} else {
-			animation.CrossFade("idle");
-		}
-	}
+public class AIAnimation : MonoBehaviour
+{
+    public float minimumRunSpeed = 1.0F;
+    public string runAnimName = "run";
+    NavMeshAgent navAgent;
+
+    void Start()
+    {
+        // Set all animations to loop
+        animation.wrapMode = WrapMode.Loop;
+
+        // Put idle and run in a lower layer. 
+        //They will only animate if our action animations are not playing
+        animation["idle"].layer = -1;
+        animation[runAnimName].layer = -1;
+
+        animation.Stop();
+        navAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        float speed = navAgent.velocity.magnitude;
+        if (speed > minimumRunSpeed)
+            animation.CrossFade(runAnimName);
+        else
+            animation.CrossFade("idle");
+    }
 }
